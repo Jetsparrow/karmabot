d2002496500462682e299b4b4cee77aae950cc8e
jetsparrow <scram.jet.sparrow@gmail.com>
2018-12-19 22:42:53 +0300

    Finish moving to Perfusion

--- JetKarmaBot/Program.cs
+++ JetKarmaBot/Program.cs
@@ -1,26 +1,61 @@
 ﻿using System;
+using System.Threading;
 using Perfusion;
 
 namespace JetKarmaBot
 {
-    public class App
+    public static class Program
     {
-        public static void Main(string[] args)
+        public enum ExitCode : int
+        {
+            Ok = 0,
+            ErrorNotStarted = 0x80,
+            ErrorRunning = 0x81,
+            ErrorException = 0x82,
+            ErrorInvalidCommandLine = 0x100
+        };
+
+#if DEBUG
+        public const bool Debug = true;
+#else
+        public const bool Debug = false;
+#endif
+        public static int Main(string[] args)
         {
             Container c = new Container();
             c.AddInstance(new Config("karma.cfg.json"));
-            Current = c.GetInstance(typeof(App)) as App;
-            Console.ReadKey();
-        }
+            c.Add<Db>();
+            c.Add<JetKarmaBot>();
 
-        public static App Current { get; private set; }
-        [Inject(true)]
-        public void Run(Container c)
-        {
-            Watcher = c.GetInstance<JetKarmaBot>();
-            Console.WriteLine("JetKarmaBot started!");
-        }
+            var bot = c.GetInstance<JetKarmaBot>();
 
-        JetKarmaBot Watcher { get; set; }
+            try
+            {
+                bot.Init().Wait();
+                Console.WriteLine("JetKarmaBot started. Press Ctrl-C to exit...");
+                Environment.ExitCode = (int)ExitCode.ErrorRunning;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                Environment.ExitCode = (int)ExitCode.ErrorException;
+            }
+            ManualResetEvent quitEvent = new ManualResetEvent(false);
+            try
+            {
+                Console.CancelKeyPress += (sender, eArgs) => // ctrl-c
+                {
+                    eArgs.Cancel = true;
+                    quitEvent.Set();
+                };
+            }
+            catch { }
+
+            quitEvent.WaitOne(Timeout.Infinite);
+            Console.WriteLine("Waiting for exit...");
+            bot?.Stop()?.Wait();
+
+            return (int)ExitCode.Ok;
+        }
     }
 }

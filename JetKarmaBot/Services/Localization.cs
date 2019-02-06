98361572dc481f3cf7e2efc03f9fccfd65a019d5
Nikolay Kochulin <porez0xfeedface@gmail.com>
2019-02-06 20:17:57 +0300

    Add /locale list and /locale all

--- JetKarmaBot/Services/Localization.cs
+++ JetKarmaBot/Services/Localization.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,7 +9,7 @@ using Perfusion;
 
 namespace JetKarmaBot
 {
-    public class Localization
+    public class Localization : IReadOnlyDictionary<string, Locale>
     {
         private Dictionary<string, Locale> locales = new Dictionary<string, Locale>();
 
@@ -77,21 +78,42 @@ namespace JetKarmaBot
 
         void Log(Exception e) => Console.WriteLine(e);
 
-        public class Locale
-        {
-            private Dictionary<string, string> locale;
-            private string localeName;
-            private string[] commonNames;
+        public IEnumerable<string> Keys => locales.Keys;
 
-            public Locale(JObject locale, string localeName)
-            {
-                this.locale = locale.Property("strings").Value.ToObject<Dictionary<string, string>>();
-                this.localeName = localeName;
-                this.commonNames = locale.Property("names").Value.ToObject<string[]>();
-            }
-            public string[] CommonNames => commonNames;
-            public string Name => localeName;
-            public string this[string name] => locale.ContainsKey(name) ? locale[name] : "unknown";
+        public IEnumerable<Locale> Values => locales.Values;
+
+        public int Count => locales.Count;
+
+        public bool ContainsKey(string key) => locales.ContainsKey(key.ToLower());
+
+        public bool TryGetValue(string key, out Locale value)
+        {
+            return locales.TryGetValue(key.ToLower(), out value);
         }
+
+        public IEnumerator<KeyValuePair<string, Locale>> GetEnumerator()
+        {
+            return locales.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+
+    }
+    public class Locale
+    {
+        private Dictionary<string, string> locale;
+        private string localeName;
+        private string[] commonNames;
+
+        public Locale(JObject locale, string localeName)
+        {
+            this.locale = locale.Property("strings").Value.ToObject<Dictionary<string, string>>();
+            this.localeName = localeName;
+            this.commonNames = locale.Property("names").Value.ToObject<string[]>();
+        }
+        public string[] CommonNames => commonNames;
+        public string Name => localeName;
+        public string this[string name] => locale.ContainsKey(name) ? locale[name] : "unknown";
     }
 }
